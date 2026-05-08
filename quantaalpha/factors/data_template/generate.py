c99585943ff1e3fe9cdd6889ee1952171e87e16a
wjh720 <1040594377@qq.com>
2026-05-08 16:46:27 +0800

    Update generate.py

--- quantaalpha/factors/data_template/generate.py
+++ quantaalpha/factors/data_template/generate.py
@@ -10,7 +10,7 @@ fields = ["$open", "$close", "$high", "$low", "$volume"]  # , "$amount", "$turn"
 data = D.features(instruments, fields, freq="day").swaplevel().sort_index().loc["2015-01-01":].sort_index()
 
 # Calculate return
-data["$return"] = data.groupby(level=0)["$close"].pct_change().fillna(0)
+data["$return"] = data.groupby(level=0)["$close"].pct_change(fill_method=None).fillna(0)
 
 print(data)
 
@@ -30,6 +30,6 @@ data = (
 )
 
 # Calculate return
-data["$return"] = data.groupby(level=0)["$close"].pct_change().fillna(0)
+data["$return"] = data.groupby(level=0)["$close"].pct_change(fill_method=None).fillna(0)
 print(data)
 data.to_hdf("./daily_pv_debug.h5", key="data")
